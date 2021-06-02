df7e683496762ba9edd9952ebcd3ca6fa826cb28
FahimFBA <fahimbinamin@gmail.com>
2021-06-02 18:39:06 +0600

    Identify whether a char is in English alphabet or not

---
+++ Problem 6/solve.py
@@ -0,0 +1,8 @@
+var = input("Enter a character: ")
+
+# condition checking
+
+if (var >= 'a' and var <= 'z' or var >= 'A' and var <= 'Z'):
+	print("It's an alphabet")
+else:
+	print("It's not an alphabet")
